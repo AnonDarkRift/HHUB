a495bf39db31f0a3a72016501c119d64fa660e9f
AnonDarkRift <86753084+AnonDarkRift@users.noreply.github.com>
2021-07-05 19:35:16 +1200

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -13,12 +13,23 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
+
+
+const account_form = document.getQuerySelector("#login-content")
+account_form.addEventListner("register-register" (e) => {
+  const email = account_form["uname"].value;
+  const password = account_form["pword"].value;
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in 
     var user = userCredential.user;
-    // ...
-  })
+    return db.collection("users").doc(user).set({
+      fname: account_form["fname"].value,
+      lname: account_form["lname"].value;
+    })
+  });
+}
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
