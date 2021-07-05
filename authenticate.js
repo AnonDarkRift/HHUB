6ada519571005ace3a71b65d7cd8053de9b64202
AnonDarkRift <86753084+AnonDarkRift@users.noreply.github.com>
2021-07-05 19:51:53 +1200

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,18 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyBT_VCGhN1rbpyN9r34W9Pyj9HiuxIam24",
-  authDomain: "hhub-database.firebaseapp.com",
-  projectId: "hhub-database",
-  storageBucket: "hhub-database.appspot.com",
-  messagingSenderId: "794199320276",
-  appId: "1:794199320276:web:569ddddc80cc9cf80cfd41",
-  measurementId: "G-GVXRNPJM3R"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 
 
 const account_form = document.getQuerySelector("#login-content")
